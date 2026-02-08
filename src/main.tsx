864c137c885191d403b5b4978aa737cacd82bc13
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-02-08 21:26:41 +0100

    UI update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./state/auth";
+import { AuthProvider } from "./state/auth.provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
